1b2156f5530fa83b2e3c9c8416d633d9a8afc7ea
Aryana Mohammadi <am5723@barnard.edu>
2024-03-28 11:42:18 -0400

    add project files

---
+++ ICARE NYC/ICARE NYC/ICARE_NYCApp.swift
@@ -0,0 +1,17 @@
+//
+//  ICARE_NYCApp.swift
+//  ICARE NYC
+//
+//  Created by Aryana Mohammadi on 3/28/24.
+//
+
+import SwiftUI
+
+@main
+struct ICARE_NYCApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
