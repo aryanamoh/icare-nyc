af827e9d22bb84a0379fad5f4200224d5e24a0ec
Aryana Mohammadi <am5723@barnard.edu>
2024-04-03 23:30:04 -0400

    add home page

--- ICARE NYC/ICARE NYC/Views/SchoolSearchButtons.swift
+++ ICARE NYC/ICARE NYC/Views/SchoolSearchButtons.swift
@@ -6,13 +6,36 @@
 //
 
 import SwiftUI
+import MapKit
 
 struct SchoolSearchButtons: View {
+    @Binding var searchResults: [MKMapItem]
+    
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        HStack {
+            Button {
+                search(for: "public school")
+            } label: {
+                Label("Schools", systemImage: "building.fill")
+            }
+            .buttonStyle(.borderedProminent)
+        }
+        .labelStyle(.iconOnly)
+    }
+    
+    
+    func search(for query: String) {
+        let request = MKLocalSearch.Request()
+        request.naturalLanguageQuery = query
+        request.resultTypes = .pointOfInterest
+        request.region = MKCoordinateRegion(
+            center: .nycCenter, span: MKCoordinateSpan(latitudeDelta: 0.4, longitudeDelta: 0.4)
+        )
+        Task {
+            let search = MKLocalSearch(request: request)
+            let response = try? await search.start()
+            searchResults = response?.mapItems ?? []
+        }
     }
 }
 
-#Preview {
-    SchoolSearchButtons()
-}
